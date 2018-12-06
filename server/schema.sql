0363da085487e3ff56c3bdc0406619a2617d21b9
tianaraemamaril <tianaraetran@gmail.com>
2018-12-05 17:32:37 -0800

    added some preliminary code to create a database

--- server/schema.sql
+++ server/schema.sql
@@ -4,10 +4,27 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  createdAt DATETIME,
+  objectID int,
+  roomname varchar(255),
+  text varchar(255),
+  updatedAt DATETIME,
+  username varchar(255),
+  PRIMARY KEY (objectID)
 );
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE username (
+  username varchar(255),
+  id int,
+  PRIMARY KEY (id)
+)
 
+CREATE TABLE roomname (
+  roomname varchar(255),
+  id int,
+  PRIMARY KEY (id)
+)
 
 
 
