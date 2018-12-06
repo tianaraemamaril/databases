3a0cb970ffe11c4f549182f74e401b229da37531
tianaraemamaril <tianaraetran@gmail.com>
2018-12-05 20:54:30 -0800

    finsihed setting up foreign keys

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,24 @@
 var mysql = require('mysql');
 
 // Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
+// You will need to connect with the user "student" and password 'student'
 // and to the database "chat".
 
+
+var connection - mysql.createConnection({
+  host: 'localhost',
+  user: 'student',
+  password: 'student',
+  port: 3000,
+  database: 'chat'
+});
+
+connection.connect(function(err) {
+  if(err) {
+    console.error('error connecting!');
+    return
+  }
+  
+  console.log('connected as id ' + connection.threadId)
+})
 
--- server/schema.sql
+++ server/schema.sql
@@ -1,32 +1,38 @@
+-- DROP DATABASE chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-  createdAt DATETIME,
-  objectID int,
-  roomname varchar(255),
-  text varchar(255),
-  updatedAt DATETIME,
-  username varchar(255),
-  PRIMARY KEY (objectID)
-);
+
 
 /* Create other tables and define schemas for them here! */
 CREATE TABLE username (
   username varchar(255),
-  id int,
-  PRIMARY KEY (id)
-)
+  usernameID int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (usernameID)
+);
 
 CREATE TABLE roomname (
   roomname varchar(255),
-  id int,
-  PRIMARY KEY (id)
-)
+  roomnameID int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (roomnameID)
+);
 
-
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  createdAt DATETIME,
+  objectID int NOT NULL AUTO_INCREMENT,
+  roomname varchar(255),
+  text varchar(255),
+  updatedAt DATETIME,
+  username varchar(255),
+  usernameID int,
+  roomnameID int,
+  PRIMARY KEY (objectID),
+  FOREIGN KEY (usernameID) REFERENCES username(usernameID),
+  FOREIGN KEY (roomnameID) REFERENCES roomname(roomnameID)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
